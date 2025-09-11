78ecde26b6424f602163d71e64118091882b720e
avery <62368112+AVERYMNZN@users.noreply.github.com>
2025-09-11 21:04:25 +0800

    Added string manager class to complement string.properties

---
+++ src/main/java/modules/StringManager.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modules;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ *
+ * @author avery
+ */
+public class StringManager {
+    private static final Properties props = new Properties();
+    
+    static {
+        try(InputStream input = StringManager.class.getClassLoader().getResourceAsStream("strings.properties")) {
+            if(input != null) {
+                props.load(input);
+            } else {
+                System.out.println("Could not find strings.properties");
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static String get(String key) {
+        String value = props.getProperty(key);
+        if (value == null) {
+            System.out.println("Missing property for key: " + key);
+            return "!" + key + "!";
+        }
+        return value;
+    }
+}
